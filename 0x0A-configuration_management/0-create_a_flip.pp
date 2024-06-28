199f40ef88a2cbb196e7f2416dd922866544096c
ezakariaa <zakariaorche@gmail.com>
2024-06-28 16:04:36 -0700

    Update

--- 0x0A-configuration_management/0-create_a_flip.pp
+++
@@ -1,7 +0,0 @@
-# This manifest creates a file at /tmp/school with specific properties
-file { '/tmp/school':
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I love Puppet',
-}
